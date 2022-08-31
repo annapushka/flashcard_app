01bb21c4a2eaa06b0fa67bc8abeb52eda3d18edf
Anna Pushkareva <smirnova_spb@mail.ru>
2022-08-31 16:37:04 +0300

    links

--- src/assets/components/WordList.jsx
+++ src/assets/components/WordList.jsx
@@ -17,7 +17,7 @@ function WordList(props) {
     function handleAdd() { setAdding(true) }
 
     function add() {
-        fetch('http://itgirlschool.justmakeit.ru/api/words/add', {
+        fetch('/api/words/add', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json;charset=utf-8'
